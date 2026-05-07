870522041a91ac5e88e7b69e10fe3a49188daccf
Natchathorn Danusawad - 6609650319 <145310503+Natchathorn@users.noreply.github.com>
2026-05-08 00:06:35 +0700

    Add layout initial test file for parcel service

---
+++ backend/service/parcel_service_test.go
@@ -0,0 +1,13 @@
+// วุ่นกัส
+package service
+
+import (
+	"context"
+	"database/sql"
+	"strings"
+	"testing"
+
+	"kencatexpress/backend/internal/domain"
+	"kencatexpress/backend/internal/dto"
+	"kencatexpress/backend/internal/util"
+)
